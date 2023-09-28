dffb9ec4ebbb792a9296d7bf6759e8d36107b62f
Allaudinkhilji <baluprakash39@gmail.com>
2023-09-28 15:02:14 +0530

    Your commit message here

--- routes/careform.js
+++ routes/careform.js
@@ -3,39 +3,7 @@ const router = express.Router();
 
 const FormData = require('../Models/careschema.js'); 
 
-router.post('/uploadcare', async (req, res) => {
-  try {
-    const {
-      Basic,
-      Nildip,
-      Ep,
-      RTI,
-      Yes,
-      No,
-      fouryears,
-      fiveyears,
-      fiveyearsPlusRSA, 
-    } = req.body;
 
-    const formData = {
-      Basic,
-      Nildip,
-      Ep,
-      RTI,
-      Yes,
-      No,
-      fouryears,
-      fiveyears,
-      fiveyearsPlusRSA, 
-    };
-
-    const createdForm = await FormData.create(formData);
-    res.status(201).json(createdForm);
-  } catch (error) {
-    console.error('Error creating form:', error);
-    res.status(500).json({ message: 'Error creating form' });
-  }
-});
 router.get('/getcare', async (req, res) => {  
 
     try {
@@ -47,10 +15,5 @@ router.get('/getcare', async (req, res) => {
         console.log(error)
     }
 });
-router.put('/updatecare/:id', (req, res) => {
-    FormData.findByIdAndUpdate(req.params.id, req.body)  
-        .then(() => res.json('care updated'))
-        .catch(err => res.status(400).json(`Error: ${err}`));
 
-});
 module.exports = router;
